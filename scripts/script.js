5037e50c73ed4153a61d0ae2411d210497d21c92
Vladislav Polonik <v_polonik@inbox.ru>
2021-01-26 14:42:05 +0300

    Homework_JS_1

---
+++ scripts/script.js
@@ -0,0 +1,98 @@
+// Задание 2
+let a1 = 5 % 3,
+	a2 = 3 % 5,
+	a3 = 5 + '3',
+	a4 = '5' - 3,
+	a5 = 75 + 'кг',
+	a6 = 785 * 653,
+	a7 = 100 / 25,
+	a8 = 0 * 0,
+	a9 = 0 / 2,
+	a10 = 89 / 0,
+	a11 = 98 + 2,
+	a12 = 5 - 98,
+	a13 = (8 + 56 * 4) / 5,
+	a14 = (9 - 12) * 7 / (5 + 2),
+	a15 = + "123",
+	a16 = 1 || 0,
+	a17 = false || true,
+	a18 = true > 0;
+console.log("Задание 2");
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+console.log(a5);
+console.log(a6);
+console.log(a7);
+console.log(a8);
+console.log(a9);
+console.log(a10);
+console.log(a11);
+console.log(a12);
+console.log(a13);
+console.log(a14);
+console.log(a15);
+console.log(a16);
+console.log(a17);
+console.log(a18);
+
+//Задание 3
+ let width  = 10,
+ 	height = 23;
+ SPryam = width * height;
+console.log("Задание 3");
+console.log(SPryam);
+
+//Задание 4
+let height4 = 10,
+	r = a7,
+	Pi = Math.PI;
+
+VCilindra = Pi * r ** 2 * height4;
+console.log("Задание 4");
+console.log(VCilindra);
+
+//Задание 5
+let r5 = 5,
+	SKruga = Pi * r5 **2;
+
+console.log("Задание 5");
+console.log(SKruga);
+
+//Задание 6
+let a = 5,
+	b = 7,
+	h = 10,
+	STrap;
+STrap = (a + b) * h /2;
+
+console.log("Задание 6");
+console.log(STrap);
+
+//Задание 7
+let S = 2000000,
+	p = 0.1,
+	years = 5,
+	Pereplata;
+Pereplata = S * p * years;
+
+console.log("Задание 7");
+console.log(Pereplata);
+
+//Задание 8
+let a_8 = 8,
+	b_8 = 3,
+	x1,
+	x2,
+	x3;
+x1 = ((16 - a_8) / 2) + b_8;
+x2 = (a_8 - (b_8 * 15)) / (b_8 - 6);
+x3 = 23780 / (1 + 2 + a_8 + b_8);
+
+console.log("Задание 8");
+console.log(x1);
+console.log(x2);
+console.log(x3);
+
+
